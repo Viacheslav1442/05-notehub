148b8898a7525a860ab4040ddd409d0b1c34572e
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-07-14 20:07:45 +0300

    index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InZpYWNoZXNsYXYudmVyYnl0c2t5aUBnbWFpbC5jb20iLCJpYXQiOjE3NTIzOTYxNjB9.g0QW5vArIBVspvGeC06F5b_Nk9FSYkaMqAdebgQPX3Y"
-export const BASE_URL = `https://notehub-public.goit.study/api`
+export const BASE_URL = `https://notehub-public.goit.study/api/docs`
 
 export const routes = {
     all: '/notes',

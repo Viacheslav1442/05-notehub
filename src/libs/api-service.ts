ff94da2cd722712c85cb188735fc5a5738834120
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-07-15 11:15:01 +0300

    api-service.ts

--- src/libs/api-service.ts
+++ src/libs/api-service.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { API_KEY, BASE_URL } from "../constants";
+const API_KEY = import.meta.env.VITE_NOTEHUB_TOKEN;
+import { BASE_URL } from '../constants';
 
 export const http = axios.create({
     baseURL: BASE_URL,

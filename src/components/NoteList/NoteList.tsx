57534a57c0cd3295f4173b4dd31a86ff1d376652
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-07-15 11:31:44 +0300

    tsx

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -4,22 +4,26 @@ import NoteItem from "./NoteItem.tsx";
 import type { ModalVariant } from "../../enums";
 
 export interface NoteListProps {
-    notes: Note[];
+    notes?: Note[];
     setCurrentNote: (note: Note) => void;
     setVariant: (variant: ModalVariant) => void;
 }
 
-const NoteList = ({ notes, setCurrentNote, setVariant }: NoteListProps) => {
+const NoteList = ({ notes = [], setCurrentNote, setVariant }: NoteListProps) => {
     return (
         <ul className={`${css.list} container`}>
-            {notes.map((note) => (
-                <NoteItem
-                    note={note}
-                    key={note.id}
-                    setVariant={setVariant}
-                    setCurrentNote={setCurrentNote}
-                />
-            ))}
+            {notes.length > 0 ? (
+                notes.map((note) => (
+                    <NoteItem
+                        note={note}
+                        key={note.id}
+                        setVariant={setVariant}
+                        setCurrentNote={setCurrentNote}
+                    />
+                ))
+            ) : (
+                <p>No notes found</p>
+            )}
         </ul>
     );
 };

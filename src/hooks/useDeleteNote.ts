965367b37e32e83a70aad2840475242db2d323b3
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-07-18 12:44:35 +0300

    tsx

--- src/hooks/useDeleteNote.ts
+++ src/hooks/useDeleteNote.ts
@@ -1,17 +1,18 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import * as noteService from '../services/noteService';
-import toast from "react-hot-toast";
+import toast from 'react-hot-toast';
 
 export const useDeleteNote = () => {
     const queryClient = useQueryClient();
 
     return useMutation<void, Error, number>({
-        mutationFn: (id) => noteService.deleteNote(id),
+        mutationFn: (id: number) => noteService.deleteNote(id),
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ['notes'] });
+            toast.success('Note deleted successfully');
         },
-        onError: (error) => {
+        onError: (error: Error) => {
             toast.error(error.message);
-        }
+        },
     });
 };

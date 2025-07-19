3607840f2a94dec28d376cbe7d6bb95446fb8e5f
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-07-19 12:02:05 +0300

    tsx

--- src/services/noteService.ts
+++ src/services/noteService.ts
@@ -2,23 +2,43 @@ import axios from 'axios';
 import type { Note, NoteCreate, NoteUpdate } from '../types/note';
 
 const instance = axios.create({
-    baseURL: 'https://notehub-public.goit.study/api/docs',
+    baseURL: 'https://notehub-public.goit.study/api',
     headers: { Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}` },
 });
 
-export const fetchNotes = (
+interface NoteHubResponse {
+    notes: Note[];
+    totalPages: number;
+}
+
+interface NoteHubSearchParams {
+    params: {
+        search?: string;
+        page: number;
+        perPage: number;
+    };
+}
+
+export async function fetchNotes(
     page: number,
     search: string
-): Promise<{ notes: Note[]; totalPages: number }> => {
-    return instance
-        .get('/notes', { params: { page, search } })
-        .then(res => {
-            return {
-                notes: res.data.notes,
-                totalPages: res.data.totalPages,
-            };
-        });
-};
+): Promise<{ notes: Note[]; totalPages: number }> {
+    const noteHubSearchParams: NoteHubSearchParams = {
+        params: {
+            page,
+            perPage: 12,
+        },
+    };
+    if (search.trim() !== "") {
+        noteHubSearchParams.params.search = search.trim();
+    }
+    const response = await axios.get<NoteHubResponse>(
+        "https://notehub-public.goit.study/api/notes/",
+        noteHubSearchParams
+    );
+
+    return response.data;
+}
 
 export const createNote = (data: NoteCreate): Promise<Note> => {
     return instance.post<Note>('/notes', data).then(res => res.data);
